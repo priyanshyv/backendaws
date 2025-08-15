e32bd5e49709b084e4bb547de572cc0d144c8088
priyanshyv <priyansh.yv7@gmail.com>
2025-08-16 00:51:43 +0530

    this

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://3.110.220.235:3000', // Your EC2 IP + frontend port
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
   await app.listen(process.env.PORT || 6000);
 }
 bootstrap();
